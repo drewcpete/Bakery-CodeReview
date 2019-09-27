9d366f0bb022bcfe336ed3a8bcbba6349058616d
Andrew <drewcpete@gmail.com>
2019-09-27 09:02:56 -0700

    create dialog with users and bread/pastry class

---
+++ model/bakery.cs
@@ -0,0 +1,25 @@
+namespace Bakery
+{
+    public class Bread
+    {
+        public int price {get; set;}
+        public int totalBread {get; set;}
+
+        public Bread()
+        {
+            price = 5;
+            totalBread;
+        }
+    }
+    public class Pastry
+    {
+        public int price {get; set;}
+        public int totalPastry {get; set;}
+
+        public Pastry()
+        {
+            price = 2;
+            totalPastry;
+        }
+    }
+}
