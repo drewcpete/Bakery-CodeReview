d0fb7657e3f5021781f118975e929cbcf88422b3
Andrew <drewcpete@gmail.com>
2019-09-27 13:27:01 -0700

    tried ascii art for a while but it looked bad and i removed it

--- program.cs
+++ program.cs
@@ -8,7 +8,7 @@ namespace Bakery
     {
         public static void Main()
         {
-            Console.WriteLine("Welcome to &rew's Bake Shop!  Today we have bread and pastries!");
+            Console.WriteLine("Welcome Our Bakery!  Today we have bread and pastries!");
             Console.WriteLine("Can i get a name for the order?");
             string orderName = Console.ReadLine();
             Console.WriteLine($"Okay, {orderName}, how many loaves of bread would you like?  They're $5 a loaf, but if you buy two, you get one for free!");
